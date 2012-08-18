2525ea7b13b3367438c8c1dd5393270d01428ec6
svenwittek <sven-wittek@web.de>
2012-08-18 13:00:06 +0200

    Erster Eintrag

---
+++ Zahlenraten/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 103);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 64);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Errate eine Zahl zwischen 1 und 1000";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Zahlenraten";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Zahlenraten/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int Eingabe, Zahl, Geraten;
+        bool Spiel;
+        public Form1();
+        Random Zufall = new Random();
+            InitializeComponent();
+    }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Spiel) //button1.Text == "OK"
+            {
+                try
+                {
+                    Eingabe = Convert.ToInt32(textBox1.Text);
+                    if (Eingabe == Zahl) ;
+                    {
+                        label1.Text = "Richtig";
+                        button1.Text = "Neues Spiel?";
+                        Spiel = false;
+                    }
+                    if (Eingabe < Zahl) label1.Text = "Zu klien";
+                    if (Eingabe > Zahl) label1.Text = "Zu groß";
+                    Geraten = Geraten + 1;
+                    Text = "Zahlenraten " + Convert.ToString(Geraten) + " mal";
+                }
+                catch
+                {
+                    label1.Text = "Keine Zahl!";
+                }
+            }
+            else //button1.Text == "Neues Spiel"
+            {
+                label1.Text = "Errate eine Zahl zwieschen 1 und 1000";
+                button1.Text = "OK";
+                textBox1.Text = "";
+                Zahl = Zufall.Next(1000) + 1;
+                Geraten = 0;
+                Spiel = true;
+            }
+         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Zahl = Zufall.Next(1000) + 1;
+            Geraten = 0;
+            Spiel = true;
+        }
+    }
+}
